b88959508e1fbb5e5e266673807c05b767e3645b
vikatimchenko <ts.2000.ts_2000.15.5@mail.ru>
2024-03-21 21:49:57 +0300

    add leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    LeafletModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kubsu-4mm-2024-map';
+
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 15,
+    center: latLng(45.019487, 39.031094)
+  };
+
+  onMapReady(map: L.Map): void {
+    console.log(map);
+  }
 }
